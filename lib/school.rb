3c1b17c6187b6995ff0a13bce3a3fe2173d19cf0
Christopher Baptiste <chrisbaptiste83@gmail.com>
2019-07-26 12:44:14 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
-# code here!
+class School 
+  attr_accessor :name, :roster
+  def initialize(name) 
+    @name = name
+    @roster = {}
+  end
+  
+  def add_student(student, grade_level)
+    @roster[grade_level]||=[]
+    @roster[grade_level]<< student
+  end 
+  
+
+  def grade(grade_level)
+@roster.each.do {|key, value| if key == grade_level return value}
+end 
+end 
+
+
+
+def sort 
+  new_hash = {}
+  @roster.each do |x, y| 
+    new_hash[x] = y.sort 
+  end 
+  new_hash
+end 
+  
+  end
+  
